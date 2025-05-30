4a35c978b9f9d70e1d73133d91cf7d36ad68f587
teste <teste@teste.com>
2025-05-30 16:14:10 -0300

    branch

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import os
 print(os.system('env'))
 
 print(f"branch: {os.getenv('INPUT_BRANCH')}")
-print(f"padrao_branch: {os.getenv('PADRAO_BRANCH')}")
+print(f"padrao_branch: {os.getenv('INPUT_PADRAO_BRANCH')}")
 
-if os.getenv("INPUT_BRANCH") in os.getenv("PADRAO_BRANCH"):
+if os.getenv("INPUT_BRANCH") in os.getenv("INPUT_PADRAO_BRANCH"):
     print("Branch está no padrão PADRAO_BRANCH")
